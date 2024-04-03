4a1512cf8769f15e04590fcf485a6edcdf46af8e
Tick <103855043+TickingEmulator@users.noreply.github.com>
2024-04-02 20:19:59 -0400

    Add files via upload

---
+++ src/main/java/frc/robot/commands/shooterCommands/angleShooter.java
@@ -0,0 +1,23 @@
+package frc.robot.commands.shooterCommands;
+import edu.wpi.first.wpilibj2.command.Command;
+
+import frc.robot.subsystems.shooterSystems.shooterArmSubsystem;
+import frc.robot.subsystems.shooterSystems.shooterRollersSubsystem;
+
+public class angleShooter extends Command {
+    private final shooterArmSubsystem m_ShooterArmSubsystem;
+    private final shooterRollersSubsystem m_ShooterRollersSubsystem;
+    public angleShooter(shooterArmSubsystem m_ShooterArmSubsystem, shooterRollersSubsystem m_ShooterRollersSubsystem){
+        this.m_ShooterArmSubsystem = m_ShooterArmSubsystem;
+        this.m_ShooterRollersSubsystem = m_ShooterRollersSubsystem;
+        addRequirements(m_ShooterArmSubsystem,m_ShooterRollersSubsystem);
+    }
+    public void initialize(){
+        // -4.499995231628418
+        m_ShooterArmSubsystem.positionArmShooter(-4.499995231628418); // abs: 0.662719615161288
+        m_ShooterRollersSubsystem.shooterMotors(true);
+    }
+    public boolean isFinished(){
+        return true;
+    }
+}
